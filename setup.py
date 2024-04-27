8c15ba424f22573a8d559f4fa1691c8ad8cb55c8
Hanna Moazam <hannamoazam@gmail.com>
2024-04-27 22:55:20 +0300

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
 
+# Read the content of the README file	
+with open('README.md', encoding='utf-8') as f:	
+    long_description = f.read()	
+
 setuptools.setup(
     name="test_pkg_hmoazam",
     version="0.0.0",
     author="Hanna",
     description="Do not use",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=["test_pkg_hmoazam"]
 )
