a8be45b187e41393d2874657e70bb620a02379ce
Hanna Moazam <hannamoazam@gmail.com>
2024-04-27 20:21:32 +0300

    Testing auto deployment to pypi. Still todo: check updates to requirements, get tag from branch and update version in setup and pyproject and generate release notes(auto)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="test_pkg_hmoazam",
-    version="0.0.2",
+    version="0.0.0",
     author="Hanna",
     description="Do not use",
     packages=["test_pkg_hmoazam"]
